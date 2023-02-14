bb92a74aa548280e17beab1e64c23eba4dcd4d98
Kartax <72432845+kartax4@users.noreply.github.com>
2023-02-14 09:33:13 +0100

    Added simple movement
    goofy ass movement

---
+++ UfoGameSlodki/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D Rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        Rigidbody2D.AddForce(movement);
+    }
+}
