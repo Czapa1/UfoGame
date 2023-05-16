84c8f4754619f958bdbb446c126821cba22c31c2
Kartax <72432845+kartax4@users.noreply.github.com>
2023-05-16 10:24:12 +0200

    Audio Manager Changes

--- UfoGameSlodki/Assets/Scripts/AudioManager.cs
+++ UfoGameSlodki/Assets/Scripts/AudioManager.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class AudioManager : MonoBehaviour
+
 {
     public Sound[] sound1;
     public Sound[] sound2;
+    public AudioSource musicSource, sfxSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- UfoGameSlodki/Assets/Scripts/Sound.cs
+++ UfoGameSlodki/Assets/Scripts/Sound.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sound : MonoBehaviour
+public class Sound
 {
+    public string name;
+    public AudioClip audioClip;
     // Start is called before the first frame update
     void Start()
     {
